1ec6d5093a5aa7edb931d572e18220d6f91d8a79
Ryan <ryangilbert7926@gmail.com>
2019-01-25 15:04:44 -0500

    Consolidate components on new workout page

--- src/pages/new-workout.js
+++ src/pages/new-workout.js
@@ -3,7 +3,21 @@ import { navigate } from 'gatsby';
 import Layout from '../components/layout';
 import WorkoutManager from '../WorkoutManager';
 
-function SetInput({ id, display }) {
+const NewWorkoutPage = () => (
+  <Layout>
+    <h3>Sup, add a new workout bb</h3>
+    {renderPresetWorkouts()}
+  </Layout>
+);
+
+function renderPresetWorkouts() {
+  return WorkoutManager.presetWorkouts.map(workout => (
+    <Workout key={workout.id} {...workout} />
+  ));
+}
+
+function Workout({ id, display }) {
+  const [showInput, setShowInput] = useState(false);
   const [weight, setWeight] = useState('');
 
   function handleSubmit(event) {
@@ -12,40 +26,6 @@ function SetInput({ id, display }) {
     WorkoutManager.addWorkout({ id, display, weight });
     navigate('/');
   }
-
-  return (
-    <div>
-      <label htmlFor="lift-value" style={{ fontSize: '1rem' }}>
-        How much did you lift?
-      </label>
-      <br />
-      <input
-        autoFocus
-        type="number"
-        value={weight}
-        onClick={e => e.stopPropagation()}
-        onChange={e => setWeight(e.target.value)}
-        name="lift-value"
-        style={{ border: '2px solid black' }}
-      />
-      <button
-        style={{
-          border: '2px solid green',
-          borderRadius: '0.5rem',
-          backgroundColor: 'white',
-          float: 'right',
-          color: 'green',
-        }}
-        onClick={handleSubmit}
-      >
-        Add
-      </button>
-    </div>
-  );
-}
-
-function Workout({ id, display }) {
-  const [showInput, setShowInput] = useState(false);
 
   return (
     <div
@@ -59,76 +39,36 @@ function Workout({ id, display }) {
       onClick={() => setShowInput(!showInput)}
     >
       <span style={{ fontSize: '1.25rem' }}>{display}</span>
-      {showInput && <SetInput id={id} display={display} />}
+      {showInput && (
+        <div>
+          <label htmlFor="lift-value" style={{ fontSize: '1rem' }}>
+            How much did you lift?
+          </label>
+          <br />
+          <input
+            autoFocus
+            type="number"
+            value={weight}
+            onClick={e => e.stopPropagation()}
+            onChange={e => setWeight(e.target.value)}
+            name="lift-value"
+            style={{ border: '2px solid black' }}
+          />
+          <button
+            style={{
+              border: '2px solid green',
+              borderRadius: '0.5rem',
+              backgroundColor: 'white',
+              float: 'right',
+              color: 'green',
+            }}
+            onClick={handleSubmit}
+          >
+            Add
+          </button>
+        </div>
+      )}
     </div>
-  );
-}
-
-function mapDisplayToId(display) {
-  return display.replace(/\s/, '_').toLowerCase();
-}
-
-function renderPresetWorkouts() {
-  return WorkoutManager.presetWorkouts.map(workout => (
-    <Workout key={workout.id} {...workout} />
-  ));
-}
-
-function NewWorkoutPage() {
-  const [display, setDisplay] = useState('');
-  const [weight, setWeight] = useState('');
-
-  function createNewWorkout() {
-    WorkoutManager.addWorkout({
-      id: mapDisplayToId(display),
-      display,
-      weight,
-    });
-    navigate('/');
-  }
-
-  return (
-    <Layout>
-      <h3>Sup, add a new workout bb</h3>
-      {renderPresetWorkouts()}
-      <div>
-        <label htmlFor="new-workout-name" style={{ fontSize: 20 }}>
-          Enter a new workout
-        </label>
-        <br />
-        <input
-          type="text"
-          value={display}
-          onChange={e => setDisplay(e.target.value)}
-          name="new-workout-name"
-          placeholder="Ex. Overhead Press"
-          style={{ border: '2px solid black' }}
-        />
-      </div>
-      <div style={{ marginTop: '10px', marginBottom: '10px' }}>
-        <label htmlFor="lift-value" style={{ fontSize: 20 }}>
-          How much did you lift?
-        </label>
-        <br />
-        <input
-          type="number"
-          value={weight}
-          onChange={e => setWeight(e.target.value)}
-          name="lift-value"
-          style={{ border: '2px solid black' }}
-        />
-      </div>
-      <button
-        style={{
-          border: '2px solid blue',
-          background: 'blue',
-          color: 'white',
-        }}
-        onClick={createNewWorkout}
-      >
-        Submit
-      </button>
-    </Layout>
   );
 }
 
