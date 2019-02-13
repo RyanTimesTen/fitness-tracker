21505d9e787d72e51fc3296d60f1377d2e338056
Ryan <ryangilbert7926@gmail.com>
2019-02-13 12:01:22 -0800

    Remove unused stuff

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,35 +1,41 @@
 import React from 'react';
-import styled, { css } from 'styled-components';
-import StyledLink from '../components/styled-link';
+import { Link } from 'gatsby';
+import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
 import Layout from '../components/layout';
 import WorkoutManager from '../WorkoutManager';
-import PlusIcon from '../assets/plus.svg';
-import SvgIcon from '../components/svg-icon';
 
-const IndexPage = () => (
-  <Layout>
-    <Header>Your Current Session</Header>
-    {WorkoutManager.workouts.map(workout => (
-      <p key={workout.id}>
-        {workout.display}&#58; {workout.weight}
-      </p>
-    ))}
-    <FAB primary to="/new-workout">
-      <FontAwesomeIcon icon="igloo" />
-    </FAB>
-  </Layout>
-);
-
-const FAB = styled(StyledLink)`
-  position: fixed;
-  bottom: 1rem;
-  right: 1rem;
-`;
+export default function IndexPage() {
+  return (
+    <Layout>
+      <Header>Your Current Session</Header>
+      {WorkoutManager.workouts.map(workout => (
+        <p key={workout.id}>
+          {workout.display}&#58; {workout.weight}
+        </p>
+      ))}
+      <FAB to="/new-workout">
+        <FontAwesomeIcon icon="plus" />
+      </FAB>
+    </Layout>
+  );
+}
 
 const Header = styled.h2`
   margin: 1rem;
 `;
 
-export default IndexPage;
+const FAB = styled(Link)`
+  position: fixed;
+  bottom: 1rem;
+  right: 1rem;
+  border-radius: 50%;
+  background-color: palevioletred;
+  color: white;
+  width: 3rem;
+  height: 3rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+`;
