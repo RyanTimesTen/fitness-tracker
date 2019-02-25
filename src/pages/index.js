b7b6c6191736b3eff608d37f363bcac1d7534b7b
Ryan <ryangilbert7926@gmail.com>
2019-02-25 10:53:47 -0500

    dont need dat flex

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,9 +13,6 @@ const FAB = styled.button`
   border: none;
   width: 3rem;
   height: 3rem;
-  display: flex;
-  justify-content: center;
-  align-items: center;
   box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
   background-color: ${props => props.theme.robinhoodGreen};
   color: white;
